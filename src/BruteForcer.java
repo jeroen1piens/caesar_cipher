c33497364e26d17590b029dbdfde13e0bd580003
jeroen1piens <jeroen.piens@outlook.com>
2024-09-15 20:06:49 +0200

    BruteForcer class created

---
+++ src/BruteForcer.java
@@ -0,0 +1,31 @@
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class BruteForcer {
+
+    private static Path inputFilePath;
+    private static int startKey = 1;
+    public static void bruteForce() {
+        System.out.println("A valid filepath to a .txt file to brute force needs to be entered.\n");
+        String encryptedText = FileManager.getTextFromFile();
+        inputFilePath = FileManager.getInputFilePath();
+        int key = startKey;
+        Cipher cipher = new Cipher(key);
+        while(Validator.isValidKey(Integer.toString(key), cipher)) {
+            cipher.setKey(key);
+            String decryptedText = cipher.decrypt(encryptedText);
+            String outputFilePath = inputFilePath.getParent().toString() + "\\" + getFileName(inputFilePath)  +
+                    "_bruteforce_with_key_" + key + ".txt";
+            FileManager.saveTextToFile(decryptedText, outputFilePath);
+            key++;
+        }
+    }
+
+    private static String getFileName(Path filePath) {
+        String fileNameWithExtension = filePath.getFileName().toString();
+        String[] strArray = fileNameWithExtension.split("\\.");
+        String fileName = strArray[0];
+        return fileName;
+    }
+
+}
